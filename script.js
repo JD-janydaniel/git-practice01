247722b2c01a31dda4c57ebe5da38534bf10cd6b
JD-janydaniel <janydaniel59@gmail.com>
2024-01-28 00:04:34 +0530

    uploaded

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-var
-remoteJSON = {"allowExternalMembers": "false", "whoCanJoin": "CAN_REQUEST_TO_JOIN"},
-    localJSON = {"whoCanJoin": "CAN_REQUEST_TO_JOIN", "allowExternalMembers": "false"};
-    
-console.log( _.isEqual(remoteJSON, localJSON) );
+const object1 = {
+    name: 'ABC',
+    address: 'India'
+  };
+      
+  const object2 = {
+    address: 'India',
+    name: 'ABC'
+  };
+      
+  JSON.stringify(object1) === JSON.stringify(object2)
+  // false
+      
+  _.isEqual(object1, object2)
+  // true
